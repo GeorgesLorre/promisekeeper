77eb38be2723cbded20ad00877e07752f2e95bdf
ruben bernardes <rubenmbernardes@gmail.com>
2018-03-26 18:04:18 +0200

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "destroying"
+
+User.destroy_all
+Witness.destroy_all
+Promise.destroy_all
+
+puts "seeding"
+
+10.times do
+  users = User.create!(email: Faker::Internet.email, password: Faker::Internet.password)
+  witnesses = Witness.new(mark_done: Faker::Boolean.boolean, account_status: Faker::Coffee.blend_name)
+  witnesses.user = users
+
+   5.times do
+  promises = Promise.new(title: Faker::Seinfeld.quote, description: Faker::Science.scientist, status: Faker::DragonBall.character, deadline: Faker::Date.forward(23))
+  promises.user = User.order("random()").first
+  promises.save
+  end
+
+  witnesses.promise = Promise.order("random()").first
+  witnesses.save
+end
+
+
