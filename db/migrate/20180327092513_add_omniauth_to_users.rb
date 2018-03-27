e10aae434dcfda2b9789cdd90a3f4e935e663753
GeorgesLorre <mr.lorre@gmail.com>
2018-03-27 13:40:28 +0200

    cleanup

--- db/migrate/20180327092513_add_omniauth_to_users.rb
+++ db/migrate/20180327092513_add_omniauth_to_users.rb
@@ -1,7 +1,6 @@
 class AddOmniauthToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :provider, :string
-    add_column :users, :uid, :string
+
     add_column :users, :facebook_picture_url, :string
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
