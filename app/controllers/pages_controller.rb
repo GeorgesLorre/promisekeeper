cc3e43f4e2f2346be27aced2773fcc960c1d4803
GeorgesLorre <mr.lorre@gmail.com>
2018-03-28 11:20:16 +0200

    koala

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
-  def home
+  # def home
+  #   @graph = Koala::Facebook::API.new(User.last.token)
+  #   @profile = @graph.get_object("me")
+  #   @friends = @graph.get_connections("me", "friends")
+  #   raise
+  # end
+  #
+  def privacy
+    render 'pages/privacy_policy'
   end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -5,7 +5,7 @@
 Devise.setup do |config|
 
   config.omniauth :facebook, ENV["FB_ID"], ENV["FB_SECRET"],
-    scope: 'email',
+    scope: 'email user_friends taggable_friends',
     info_fields: 'email, first_name, last_name',
     image_size: 'square',  # 50x50, guaranteed ratio
     secure_image_url: true
