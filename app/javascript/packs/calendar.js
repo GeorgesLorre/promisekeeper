d17c57479b6565734d3dd4cd75a06f9b64e6117e
GeorgesLorre <mr.lorre@gmail.com>
2018-04-04 14:38:10 +0200

    datepicker call

--- app/javascript/packs/calendar.js
+++
@@ -1,8 +0,0 @@
-// $('.deadline').pickdate()
-import {pickadate} from 'pickadate.js';
-
-function datepicker() {
-  console.log("inside datepicker function");
-   $('.datepicker').pickadate();
- };
- export {datepicker};
